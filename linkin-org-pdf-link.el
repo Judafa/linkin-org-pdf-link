ac0890d43a89614d80fc9750664e2d4584cffe2a
Julien Dallot <judafa@protonmail.com>
2025-08-12 15:51:37 +0200

    init pdf link

---
+++ linkin-org-pdf-link.el
@@ -0,0 +1,249 @@
+;;; linkin-org.el --- an emacs workflow with fast, reliable links -*- lexical-binding: t -*-
+
+;; Copyright 2025 Julien Dallot
+
+;; Author: Julien Dallot <judafa@protonmail.com>
+;; Maintainer: Julien Dallot <judafa@protonmail.com>
+;; URL: https://github.com/Judafa/linkin-org
+;; Version: 1.0
+;; Package-Requires: ((emacs "30.1") (pdf-tools "1.1.0"))
+
+;; This file is not part of GNU Emacs
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License. 
+
+;;; Commentary:
+;; linkin-org proposes to access your data with reliable links to place your written notes at the center of your workflow.
+;; The links work fast and are easy to create; most importantly, the links are reliable and can robustly support a whole link-based workflow.
+
+(require 'ol)
+(require 'dired)
+(require 'pdf-tools)
+
+
+
+(defun org-pdf-open (link)
+  "Open a LINK in string form with type pdf."
+  (let*
+      (
+	   (link-parts (split-string link "::"))
+	   (file-path (car link-parts))
+       (metadata (when (cadr link-parts)
+                   (read (cadr link-parts))))
+       (pdf-file (car link-parts))
+       (page
+        (if (and (plistp metadata) (= 2 (length link-parts)))
+            ;; if the link is with the plist format
+            (plist-get metadata :page)
+          ;; else if the data is just separated by ::
+          (string-to-number (car (cdr link-parts)))))
+       
+	   (edges-list
+        (if (and (plistp metadata) (= 2 (length link-parts)))
+            ;; if the link is with the new plist format
+            (let*
+	            (
+                 (edges-raw-str (plist-get metadata :edges))
+                 (edges-list-str (when edges-raw-str
+                                   (split-string edges-raw-str ";")))
+                 )
+	          ;; convert from string to int, get a list of four floating points numbers, that's the edges
+	          (when edges-list-str (list (mapcar #'string-to-number edges-list-str))))
+          ;; else if the data is just separated by ::
+	      (let*
+              (
+               (edges-str (car (cdr (cdr link-parts))))
+	           ;; separate the edges by |
+	           (edges-list-str (when edges-str (split-string edges-str "[;|]"))))
+	        ;; convert from string to int, get a list of four floating points numbers, that's the edges
+	        (when edges-list-str (list (mapcar #'string-to-number edges-list-str))))))
+	   ;; (path+page+edges (split-string link "::"))
+	 ;; ;; for the pdf file path
+     ;;     (pdf-file (car path+page+edges))
+	 )
+    ;; (start-process "view-pdf" nil "zathura" pdf-file (format "--page=%s" page))))
+    (progn
+      ;; (message edges-list)
+      ;; check if the pdf file is already open
+      (if-let (
+	       ;; get the buffer of the pdf file
+	       (pdf-buffer (get-file-buffer pdf-file))
+
+	       ;; check if the buffer is visible
+	       (pdf-window (get-buffer-window pdf-buffer 'visible))
+
+               ;; Trouve toutes les fenêtres qui affichent le buffer donné en entrée
+               (windows (delq (selected-window)
+                              (get-buffer-window-list
+                               pdf-buffer 'nomini t)))
+	       
+               ;; la première de ces fenêtres
+               (fenetre-finale (car windows))
+	       
+               ;; On initialise le temps le plus récent avec la temps de la première fenêtre de la liste
+               (temps-le-plus-recent (window-use-time fenetre-finale))
+
+	       ;; (not linkin-org-open-org-link-other-frame)
+	       )
+	       ;; (pdf-buffer (get-file-buffer pdf-file))
+	       ;; ;; check if the buffer is visible
+	       ;; (pdf-window (get-buffer-window pdf-buffer 'visible))
+			  ;; )
+	      ;; then dont open a new frame, rather switch to the last visisted window and highlight the edges
+	      (progn
+                   ;; Parmi celles qui affichent le buffer, séléctionne la fenêtre la plus récement utilisée
+                   (dolist (fenetre (cdr windows))
+                     (if (> (window-use-time fenetre) temps-le-plus-recent)
+                         (progn
+                           (setq temps-le-plus-recent (window-use-time fenetre))
+                           (setq pdf-window fenetre))))
+
+		   (let (
+			 (initial-window (selected-window)))
+		     ;; switch to the frame
+		     ;; (select-frame-set-input-focus (window-frame pdf-window) t)
+		    ;; (select-frame (window-frame pdf-window))
+		    ;; switch to the window
+		    (select-window pdf-window)
+
+		    ;; (if edges-list
+		    ;;     ;; if the place to highlight is provided, then make sure that place is visible (ie, pdf is scrolled so that one can see it)
+		    ;;     (let
+		    ;; 	;; idk then do the same here
+		    ;; 	;; [[file:~/.config/emacs/straight/repos/pdf-tools/lisp/pdf-occur.el::290][[file] pdf-occur.el_at_290]]
+		    ;; 	(
+		    ;; 	 (pdf-isearch-batch-mode t)
+		    ;; 	 (pixel-match (pdf-util-scale-relative-to-pixel edges-list))
+		    ;; 	 )
+		    ;;       (pdf-isearch-focus-match-batch pixel-match)
+		    ;;     
+		    ;;       (pdf-isearch-hl-matches pixel-match nil t)
+		    ;;       )
+		    ;;   ;; else, just go to the page
+		    ;;   (unless (not page)
+		    ;;     (pdf-view-goto-page page)
+		    ;;     )
+		    ;;   )
+
+
+		    ;; go to the page
+		    (when page
+		      (pdf-view-goto-page page))
+		    ;; ;; hightlight the edges
+		    (when edges-list
+		      (let
+			  ;; idk then do the same here
+			  ;; [[file:~/.config/emacs/straight/repos/pdf-tools/lisp/pdf-occur.el::290][[file] pdf-occur.el_at_290]]
+			  (
+			   (pdf-isearch-batch-mode t)
+			   (pixel-match (pdf-util-scale-relative-to-pixel edges-list)))
+			;; dont forget to scale the edges to the current display!
+			(pdf-isearch-hl-matches pixel-match nil t)
+			(pdf-isearch-focus-match-batch pixel-match)))
+		    ;; switch back to the initial buffer
+		    (select-window initial-window)))
+	;; if the pdf file is not visible, open a new frame
+	;; or if I specifically asked to open a new frame for the pdf file
+	    (progn
+	      ;; (message "not visible")
+	      (clone-frame)
+	      (find-file pdf-file)
+	      (pdf-view-goto-page page))
+	    ;; (if linkin-org-open-pdf-link-other-frame
+	    ;; 	  (progn
+	    ;; 	    (find-file-other-frame pdf-file)
+	    ;; 	    (pdf-view-goto-page page)
+	    ;; 	    )
+	    ;; 	)
+	    ))))
+
+;; pour copier un lien vers le fichier pdf courant
+(defun linkin-org-pdf-get-link ()
+  "Return a link in string form to the pdf in the current buffer.
+Highlighted text is included in the link."
+  (other-window 1)
+  (pdf-tools-assert-pdf-buffer)
+  (let* (
+	 (page (number-to-string (pdf-view-current-page)))
+         (file (abbreviate-file-name (pdf-view-buffer-file-name)))
+	 (file-name (file-name-nondirectory file))
+	 (file-name-sans-id (linkin-org-strip-off-id-from-file-name file-name))
+	 (file-name-sans-ext (file-name-sans-extension file-name-sans-id))
+	 (file-name-extension (file-name-extension file-name-sans-id))
+	 (nom-fichier-tronque (if (> (length file-name-sans-ext) 70)
+				  (concat (substring file-name-sans-ext 0 50)
+					  "[___]"
+					  (if file-name-extension
+					      (concat "." file-name-extension)))
+				file-name-sans-id))
+	 ;; for selected text
+	 (selected-text (if (pdf-view-active-region-p)
+			    ;; delete the newlines
+			    (replace-regexp-in-string "\n" " " (car (pdf-view-active-region-text)))
+			  nil))
+
+	 ;; ;; truncate the selected text
+	 ;; (selected-text (if (and selected-text (> (length selected-text) 15))
+	 ;; 			     (concat (substring selected-text 0 15) "...")
+	 ;; 			   selected-text
+	 ;; 			   )
+	 ;; 			 )
+
+	 ;; for the edges
+	 (edges (if (pdf-view-active-region-p)
+                ;; the edges that were really hovered by the mouse.
+                ;; not necessarily the visual highlightings, that are wordwise by default
+                ;; (pdf-info-getselection
+                ;;  (string-to-number page)
+                ;;  pdf-view-active-region
+                ;;  pdf-view-selection-style
+                ;;  )
+              
+                (mapcar
+                 (lambda (edges)
+                   (pdf-info-getselection
+                    (string-to-number page)
+                    edges
+                    pdf-view-selection-style))
+                 pdf-view-active-region)
+		      nil)))
+    (other-window 1)
+
+    ;; deselect the text, if there is an active region
+    (if (pdf-view-active-region-p)
+	(pdf-view-deactivate-region))
+
+    (if (and selected-text edges)
+	(progn
+	 (let*
+	     (
+	      ;; edges are actually outputed as a list of list of Lists-trees
+	      (edges (car (car edges)))
+	      ;; concat the edges with |
+	      (string-edges (concat "\"" (mapconcat #'prin1-to-string edges ";") "\"" )))
+
+	   ;; (format "[[pdf:%s::%s::%s][[pdf] %s _ p%s _ \"%s\"]]" file page string-edges nom-fichier-tronque page selected-text)
+	   ;; without the pdf name
+	   ;; (format "[[pdf:%s::%s::%s][[pdf] p%s _ \"%s\"]]" file page string-edges page selected-text)
+	   (format "[[pdf:%s::(:page %s :edges %s)][[pdf] p%s _ \"%s\"]]" file page string-edges page selected-text)))
+      ;; else, no selected text, just care about the path and page
+      ;; (format "[[pdf:%s::%s][[pdf] %s _ p%s]]" file page nom-fichier-tronque page)
+      (format "[[pdf:%s::(:page %s)][[pdf] %s _ p%s]]" file page nom-fichier-tronque page))))
+
+
+
+;;;; add the link type
+(let ((inhibit-message t)) ;; dont print messages while loading the package
+  (org-add-link-type "pdf" 'org-pdf-open nil))
+
+(provide 'linkin-org-pdf-link-type)
